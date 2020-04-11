2b373a3c9885cfe41def1862d854e32ec00e2f30
Mike <35762919+feifeidadi@users.noreply.github.com>
2020-04-10 23:40:06 -0400

    Mofei's first pygame

---
+++ games/alien_invasion.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings 
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    #ai_settings = Settings(1200, 800, (0, 245, 245))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+
+    pygame.display.set_caption("Hello pygame!")
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+
+
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        #for bullet in bullets.copy():
+        #    if bullet.rect.bottom <= 0:
+                #bullets.remove(bullet)
+
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+
+run_game()
---
+++ games/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0,245,245)
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 250, 0, 0
+        self.ship_speed_factor = 1.5
